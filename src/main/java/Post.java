1b672271dbfebf66dbda04b720fab70100be3d3e
gluk2309 <gluk2309@mail.ru>
2023-08-09 12:43:24 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public FormDate birthday = new FormDate();
+
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;// добавьте другие поля
+}
